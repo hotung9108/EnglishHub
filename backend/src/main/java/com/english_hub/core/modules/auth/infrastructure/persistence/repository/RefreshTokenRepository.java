cf7b54024e5df8c4901b59c4aa9254a5c61c6c16
tuanpham21105 <tuan.phamminh9936@gmail.com>
2026-09-20 15:54:05 +0700

    fix(backend): Naming an explicit Bean name to RefreshTokenRepository JPA repo

--- backend/src/main/java/com/english_hub/core/modules/auth/infrastructure/persistence/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/english_hub/core/modules/auth/infrastructure/persistence/repository/RefreshTokenRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository("authRefreshTokenRepository")
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenJpaEntity, Long> {
 
 	@Query("select rt from RefreshToken rt where rt.tokenHash = :tokenHash")
